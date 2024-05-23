9ec62a6a5ab1fa2dd102233a25f0f1405188b309
Youngrok Lee <cakeil@gmail.com>
2024-05-22 21:47:11 -0400

    add log

---
+++ set_logs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+BRANCH="main"
+
+# Save the current time into a log file
+date >> ./log.txt
+
+# Commit
+p=$(pwd)
+git config --global --add safe.directory $p
+
+if [[ "$(git status --porcelain)" != ""]]; then
+    git config --global user.name $USER_NAME
+    git config --global user.email $USER_EMAIL
+    git add ./log.txt
+    git commit -m "Update the log"
+    git push origin $BRANCH
+else
+echo "Nothing to commit..."
+fi
